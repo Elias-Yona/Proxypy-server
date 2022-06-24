f88f074d49ccdcedc4ef36332dab48fdb1c67f5c
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2022-06-24 18:28:58 +0300

    still experimenting

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import proxy
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 26393))
+    _port = int(os.environ.get('PORT', 5000))
     proxy.main(
         hostname=ipaddress.IPv4Address('0.0.0.0'),
-        port=port
+        ports = _port
     )
