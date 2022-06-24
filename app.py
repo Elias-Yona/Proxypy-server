2075db9c59fca25e8aa36a1e934a268f7bcbebd4
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2022-06-24 17:19:04 +0300

    Experiment: Checking if flask isn't a dependecy

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 import os
-import subprocess
+import ipaddress
+import threading
 
 from flask import Flask
+import proxy
 
 app = Flask(__name__)
 
+def start_proxy_server():
+     proxy.main(
+        hostname=ipaddress.IPv4Address('0.0.0.0'),
+        port=5000
+    )
 
-@app.route("/start")
-def start_proxy():
-    # proxy.main(
-    #     hostname=ipaddress.IPv4Address('0.0.0.0'),
-    #     port=5000
-    # )
-    subprocess.run("proxy --hostname 0.0.0.0 --port 5000")
+# @app.route("/start")
+# def start_proxy():
+#     # subprocess.run("proxy --hostname 0.0.0.0 --port 5000")
+#     # x = threading.Thread(target=start_proxy_server)
+#     # x.start()
+#     proxy.main(
+#         hostname=ipaddress.IPv4Address('0.0.0.0'),
+#         port=5000
+#     )
+#     return "proxy started successfully"
 
-    return "proxy started successfully"
 
-
-if "__name__" == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+# #     port = int(os.environ.get('PORT', 5000))
+# #     app.run(host='0.0.0.0', port=port)
+#     start_proxy_server()
+ 
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,3 @@
-from app import app
-import os
-
-if "__name__" == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-
-    app.run(host='0.0.0.0', port=port)
+import app
+  
+app.start_proxy_server()
